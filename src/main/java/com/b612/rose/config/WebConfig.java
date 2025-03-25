8abec250c6216806158cb5f25b08f63d2ad8a23b
platinum57 <sook8986@naver.com>
2025-03-26 01:34:00 +0900

    fix: FE local에서 cors오류 발생 안 하게 수정

--- src/main/java/com/b612/rose/config/WebConfig.java
+++ src/main/java/com/b612/rose/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://b612-blue.vercel.app")
+                .allowedOrigins("https://b612-blue.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
