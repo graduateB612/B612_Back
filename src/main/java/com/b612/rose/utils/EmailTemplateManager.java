f1862346c4cccc879b5e2dcda617931cd2cce082
platinum57 <sook8986@naver.com>
2025-04-09 20:04:16 +0900

    feat: 임시 이메일 템플릿 작성

---
+++ src/main/java/com/b612/rose/utils/EmailTemplateManager.java
@@ -0,0 +1,52 @@
+package com.b612.rose.utils;
+
+import com.b612.rose.entity.domain.Star;
+import com.b612.rose.entity.domain.User;
+import com.b612.rose.entity.enums.StarType;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EmailTemplateManager {
+    private final Map<String, String> npcEmailMap = new HashMap<>();
+    private final Map<String, StarType> npcStarTypeMap = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        npcEmailMap.put("어린왕자", "little_p@b612_rose.com");
+        npcEmailMap.put("장미", "rose@b612_rose.com");
+        npcEmailMap.put("여우", "prettycutyfox@b612_rose.com");
+        npcEmailMap.put("바오밥", "baobob123@b612_rose.com");
+
+        npcStarTypeMap.put("어린왕자", StarType.PRIDE);
+        npcStarTypeMap.put("장미", StarType.ENVY);
+        npcStarTypeMap.put("여우", StarType.SAD);
+        npcStarTypeMap.put("바오밥", StarType.LONELY);
+    }
+
+    public String getSenderEmail(String npcName) {
+        return npcEmailMap.getOrDefault(npcName, "noreply@b612_rose.com");
+    }
+
+    public StarType getStarTypeForNpc(String npcName) {
+        return npcStarTypeMap.get(npcName);
+    }
+
+    public String getSubject(String npcName, String purifiedTypeName) {
+        return npcName + "이 보낸 선물 - " + purifiedTypeName + "의 별";
+    }
+
+    public String getEmailContent(User user, String npcName, Star star) {
+        String purifiedTypeName = star.getPurifiedType().getDescription();
+
+        String content = "<div style='font-family: Arial, sans-serif;'>" +
+                "<h2>안녕하세요, " + user.getUserName() + "님!</h2>" +
+                "<p>" + npcName + "가 보내는 " + purifiedTypeName + "의 별입니다.</p>" +
+                "</div>";
+
+        return content;
+    }
+}
