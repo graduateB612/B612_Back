751e31ffc504488826c2f86b481f2ab8ccd821e4
platinum57 <sook8986@naver.com>
2025-09-11 01:17:06 +0900

    chore: client 단에서 넘어오는 concern을 로그에 남김

--- src/main/java/com/b612/rose/controller/GameCompletionController.java
+++ src/main/java/com/b612/rose/controller/GameCompletionController.java
@@ -4,6 +4,7 @@ import com.b612.rose.dto.request.EmailRequest;
 import com.b612.rose.dto.response.GameStateResponse;
 import com.b612.rose.service.service.GameCompletionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/game")
 @RequiredArgsConstructor
+@Slf4j
 public class GameCompletionController {
 
     private final GameCompletionService gameCompletionService;
@@ -20,6 +22,7 @@ public class GameCompletionController {
     public ResponseEntity<GameStateResponse> completeGameAndSendEmail(
             @PathVariable UUID userId,
             @RequestBody EmailRequest request) {
+        log.info("클라이언트 concern 수신: {}", request.getConcern());
         GameStateResponse response = gameCompletionService.completeGameAndSendEmail(userId, request);
         return ResponseEntity.ok(response);
     }
